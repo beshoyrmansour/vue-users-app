0eb129c3e70ab79e1d9189df919246f3bb6f1d99
Beshoy R Mansour <bmansour@iotbule.com>
2022-07-18 19:19:47 +0200

    Add Search

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -2,11 +2,15 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
-      </div>
       <h1 class="mb-0 pb-0">Users</h1>
+      <div class="row row-cols-1 my-4 pb-0">
+        <div class="form-group ">
+          <label for="query">Search</label>
+          <input class="form-control" id="query" v-model="query" type="text" placeholder="Search by Name">
+        </div>
+      </div>
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 mt-0">
-        <User v-for="user in $store.getters.usersWithImages" :key="user.id" :user="user" />
+        <User v-for="user in usersWithImages" :key="user.id" :user="user" />
       </div>
     </div>
   </div>
@@ -19,6 +23,18 @@ export default {
   components: {
     User
   },
+  data() {
+    return {
+      query: ''
+    }
+  },
+  computed: {
+    usersWithImages() {
+      return this.$store.getters.usersWithImages.filter(user => {
+        return user.name.toLowerCase().includes(this.query.toLowerCase())
+      })
+    }
+  }
 
 }
 </script>
